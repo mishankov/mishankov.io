24bd51281e91154d4b85c3c6cad6fe74bd9c27e4
mishankov <mishankov@mail.com>
2019-01-27 16:33:06 +0300

    remove sakura. using skeleton now

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mishankov.io
 Personal website
+
+Core CSS by http://getskeleton.com/
+Patterns by http://thepatternlibrary.com/
+Image hover effects by http://imagehover.io/
+Icons by https://fontawesome.com/
--- source/static/scripts/homepage.js
+++ source/static/scripts/homepage.js
@@ -1,11 +1,11 @@
-BLOCK_LIST = ['aboutme', 'job', 'github', 'instagram', 'soundcloud', 'footer'];
-CURRENT_POSITION = '';
+// BLOCK_LIST = ['aboutme', 'job', 'github', 'instagram', 'soundcloud', 'footer'];
+// CURRENT_POSITION = '';
 
 
-window.onload = function() {
-	CURRENT_POSITION = BLOCK_LIST[0];
+// window.onload = function() {
+// 	CURRENT_POSITION = BLOCK_LIST[0];
 	// CURRENT_POSITION = 'soundcloud';
-	location.hash = CURRENT_POSITION;
+	// location.hash = CURRENT_POSITION;
 
 	// for (let i=0; i<BLOCK_LIST.length; i++) {
 	// 	element = document.getElementsByClassName('hp-'+BLOCK_LIST[i])[0];
@@ -25,9 +25,9 @@ window.onload = function() {
 
 	// 	console.log(element);
 	// }
-}
+// }
 
-function scrollToBlock(block) {
-	console.log(block)
-	location.hash = block
-}
+// function scrollToBlock(block) {
+// 	console.log(block)
+// 	location.hash = block
+// }
